e4b9b0949b5743f77fc3bbbbdec89a98a14b44c9
fedstrial <fedstrial@proton.me>
2025-11-07 18:00:35 +0100

    add basic layout with button

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,7 +1,18 @@
+import { useLocation, Link } from "react-router-dom";
+
 export default function MyHeader() {
+	const location = useLocation();
+
 	return (
-		<header>
-			<a>BoolRoad</a>
+		<header className="d-flex justify-content-between align-items-center my-4">
+			<h1 className="display-1">
+				<a href="/">BooRoad</a>
+			</h1>
+			{location.pathname === "/users" && (
+				<Link to="/" className="btn btn-primary">
+					Torna alla Homepage
+				</Link>
+			)}
 		</header>
 	);
 }
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -3,11 +3,11 @@ import MyHeader from "../components/MyHeader";
 
 export default function DefaultLayout() {
 	return (
-		<>
+		<div className="container-wide">
 			<MyHeader />
 			<main>
 				<Outlet />
 			</main>
-		</>
+		</div>
 	);
 }
